bfc4d60106c187af7397c3f4dc4499de3c0c28bb
Dannny <danielpradeep0@gmail.com>
2023-01-24 18:40:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, CircularProgress, TextField } from "@mui/material";
+import axios from "axios";
+import moment from "moment/moment";
+import React, { useState } from "react";
+import Table from "./Table";
 
 function App() {
+  const [location, setLocation] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleSearch = () => {
+    setIsLoading(true);
+    const geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=1635890035cbba097fd5c26c8ea672a1`;
+    axios.get(geoApi).then((response) => {
+      console.log(response);
+
+      const { lat, lon } = response?.data[0];
+      console.log(lat, lon);
+      const forecastApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1635890035cbba097fd5c26c8ea672a1`;
+
+      axios.get(forecastApi).then((response) => {
+        console.log(response?.data?.list);
+        setData(response?.data?.list);
+        setIsLoading(false);
+      });
+    });
+  };
+
+  const dateString = moment(1674572400).format("MM/DD/YYYY");
+
+  console.log(dateString);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__title">
+          <h3 className="title">Weather in your city</h3>
+        </div>
+
+        <div className="header__center">
+          <TextField
+            className="TextField"
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            hiddenLabel
+            defaultValue="Small"
+            size="small"
+            borderColor="black"
+            color="warning"
+            required
+            sx={{
+              marginRight: 4,
+            }}
+          />
+
+          <div className="divider"></div>
+
+          <Button
+            variant="contained"
+            color="warning"
+            sx={{
+              color: "white",
+              backgroundColor: "orange",
+              borderColor: "orange",
+              marginRight: 10,
+            }}
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+
+          {isLoading ? (
+            <CircularProgress
+              sx={{
+                color: "#ffa500",
+                marginLeft: 4,
+              }}
+            />
+          ) : (
+            <div style={{ width: 60 }}></div>
+          )}
+        </div>
       </header>
+
+      <Table data={data} />
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Table.css";
+
+const Table = ({ data }) => {
+  return (
+    <div className="table_container">
+      {data.map((details, i) => (
+        <table>
+          <tr>
+            <th colspan="2" style={{ backgroundColor: "#ffa500" }}>
+              Date: {details.dt_txt}
+            </th>
+          </tr>
+          <tr>
+            <th colspan="2">Temrature</th>
+          </tr>
+          <tr>
+            <th>Min</th>
+            <th>Max</th>
+          </tr>
+          <tr>
+            <td>{details?.main?.temp_min}</td>
+            <td>{details?.main?.temp_max}</td>
+          </tr>
+          <tr>
+            <td>Pressure</td>
+            <td>{details?.main?.pressure}</td>
+          </tr>
+          <td>Humadity</td>
+          <td>{details?.main?.humidity}</td>
+        </table>
+      ))}
+    </div>
+  );
+};
+
+export default Table;
